c02683076816504f09c1190410abb872351c3bd2
Andre Fernandes <andre@vertigo.com.br>
2025-06-28 06:36:42 -0300

    catalog-info

--- refresh_catalog.sh
+++ refresh_catalog.sh
@@ -2,6 +2,6 @@
 
 # This script will refresh the catalog by running the following commands:
 # TODO: testar, deveria usar o webhook do backstage
-curl -X POST \
+curl -vv -X POST \
   "https://control-plane.saas.vee.codes/api/events/http/github" \
   -H "Authorization: Bearer $GITHUB_WEBHOOK_TOKEN"
